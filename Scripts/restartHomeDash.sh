4c3f0c8c627f9735e323643eaec0291a55032726
Jeetah <fortbild@streber24.de>
2021-12-19 22:36:41 +0100

    Initial commit

---
+++ Scripts/restartHomeDash.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+set -ex
+sudo systemctl restart homedash.service
---
+++ Scripts/startHomeDash.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#set -eu
+
+xset -dpms     # disable DPMS (Energy Star) features.
+xset s off     # disable screen saver
+xset s noblank # don't blank the video device
+matchbox-window-manager -use_titlebar no &
+unclutter &    # hide X mouse cursor unless mouse activated
+chromium-browser --display=:0 --kiosk --window-position=0,0 http://127.0.0.1:8123
