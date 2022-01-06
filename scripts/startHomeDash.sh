05c8bceaca5e4c82e7702705c8acd8ad1723623f
Jeetah <fortbild@streber24.de>
2022-01-06 11:14:53 +0100

    correct dashboard url

--- scripts/startHomeDash.sh
+++ scripts/startHomeDash.sh
@@ -6,4 +6,4 @@ xset s off     # disable screen saver
 xset s noblank # don't blank the video device
 matchbox-window-manager -use_titlebar no &
 unclutter &    # hide X mouse cursor unless mouse activated
-chromium-browser --display=:0 --kiosk --window-position=0,0 http://127.0.0.1:8123
+chromium-browser --display=:0 --kiosk --window-position=0,0 http://127.0.0.1:8123/lovelace-homedash/0
