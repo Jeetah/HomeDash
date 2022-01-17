e33dc4f2ee36c7251de16b3f4f7059e13efaba6a
Jeetah <fortbild@streber24.de>
2022-01-17 10:47:49 +0100

    backup script

--- shell_scripts/backup2sdcard.sh
+++ shell_scripts/backup2sdcard.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # set -euo pipefail
-sudo rsync -av --progress --mkpath /usr/share/hassio/backup "/mnt/sdcard/backups/hassio/backup"
+sudo rsync -av --progress --mkpath /usr/share/hassio/backup "/mnt/sdcard/backups/hassio"
